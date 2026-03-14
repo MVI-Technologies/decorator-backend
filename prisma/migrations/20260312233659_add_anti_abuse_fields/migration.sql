b509ccb70c257e2b541fa62f89f2825954ba4136
Matheus-Imberio <matheusimberio030@gmai.com>
2026-03-14 16:22:15 -0300

    chore: Delete an unspecified file.

--- prisma/migrations/20260312233659_add_anti_abuse_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "messages" ADD COLUMN     "block_reason" TEXT,
-ADD COLUMN     "is_blocked" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "professional_profiles" ADD COLUMN     "instagram" TEXT;
